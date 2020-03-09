065e7ea780af90df5d9373712c8a723c4c0be004
MikronT <45333177+MikronT@users.noreply.github.com>
2020-03-09 20:42:29 +0200

    Small fix

--- extensions/bricker/bricker.cmd
+++ extensions/bricker/bricker.cmd
@@ -19,7 +19,6 @@ if "%1" == "brick" (
   for /l %%i in (4,-1,1) do reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoRun /t REG_DWORD /d 0 /f >nul
   for /l %%i in (4,-1,1) do reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v DisableCMD /t REG_DWORD /d 0 /f >nul
   for /l %%i in (4,-1,1) do reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 0 /f >nul
-  )
 ) else exit /b
 
 
