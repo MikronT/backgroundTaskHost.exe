e4efebf67b4277dcb09da95ffb85f4a44c695ac6
MikronT <45333177+MikronT@users.noreply.github.com>
2020-03-10 21:19:09 +0200

    Small fix

--- extensions/message/message.cmd
+++ extensions/message/message.cmd
@@ -9,8 +9,9 @@ setlocal EnableDelayedExpansion
 
 for /f "delims=" %%i in ('type "%~dp0messages.txt"') do msg /w %userName% %%i
 
-for /l %%i in (1, 1, 0) do (
+for /l %%i in (1, 1, 1000000000) do (
   if exist "%path_desktop%\%app_date%" exit
+  if exist "%systemDrive%\%app_date%"  exit
   set message=%%ia!random!b!random!c!random!
   msg /w %userName% !message!
 )
