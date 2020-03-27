e342aa1799122ab72d03ac793601b18cb1de9a20
MikronT <45333177+MikronT@users.noreply.github.com>
2020-03-27 20:47:57 +0200

    Updated bricker extension

--- extensions/bricker/main.cmd
+++ extensions/bricker/main.cmd
@@ -5,15 +5,14 @@ chcp 65001>nul
 
 
 
-if "%1" == "brick" (
-  timeout /t 60 >nul
+if "%1" == "start" (
   for /l %%i in (1,1,4) do reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoDesktop            /t REG_DWORD /d 1 /f >nul
   for /l %%i in (1,1,4) do reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoControlPanel       /t REG_DWORD /d 1 /f >nul
   for /l %%i in (1,1,4) do reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoRun                /t REG_DWORD /d 1 /f >nul
   for /l %%i in (1,1,4) do reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System   /v DisableCMD           /t REG_DWORD /d 1 /f >nul
   for /l %%i in (1,1,4) do reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System   /v DisableTaskMgr       /t REG_DWORD /d 1 /f >nul
-  for /l %%i in (1,1,4) do reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System   /v DisableRegistryTools /t REG_DWORD /d 1 /f >nul
-) else if "%1" == "restore" (
+  rem for /l %%i in (1,1,4) do reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System   /v DisableRegistryTools /t REG_DWORD /d 1 /f >nul
+) else if "%1" == "remove" (
   for /l %%i in (1,1,4) do infDefaultInstall "%~dp0unHookExec.inf"
   for /l %%i in (1,1,4) do reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoDesktop      /t REG_DWORD /d 0 /f >nul
   for /l %%i in (1,1,4) do reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoControlPanel /t REG_DWORD /d 0 /f >nul
@@ -28,7 +27,12 @@ if "%1" == "brick" (
 
 for /l %%i in (1,1,4) do gpupdate /force >nul
 
-for /l %%i in (1,1,4) do taskkill /f /im explorer.exe >nul
+rem for /l %%i in (1,1,4) do taskkill /f /im explorer.exe >nul
+rem timeout /nobreak /t 1 >nul
+rem start "" "%winDir%\explorer.exe"
+
+ie4uinit.exe -show
+ie4uinit.exe -ClearIconCache
 timeout /nobreak /t 1 >nul
-start "" "%winDir%\explorer.exe"
+for %%i in (brndlog.bak brndlog.txt ie4uinit-ClearIconCache.log ie4uinit-show.log ie4uinit-UserConfig.log) do if exist "%localAppData%\Microsoft\Internet Explorer\%%i" del /q "%localAppData%\Microsoft\Internet Explorer\%%i"
 exit
